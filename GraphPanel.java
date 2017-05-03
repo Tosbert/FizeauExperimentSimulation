5d75f3ff205b328f6991f2c9fbf3fd465eeca806
AntoninaPater <spiewajacy.pies@gmail.com>
2017-05-03 14:15:28 +0200

    Add new graph panel

---
+++ GraphPanel.java
@@ -0,0 +1,15 @@
+package pl.edu.pw.fizyka.pojava.tosbert;
+
+import java.awt.Color;
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class GraphPanel extends JPanel {
+
+	public GraphPanel() {
+		
+		setBackground(Color.PINK);
+	}
+
+}
