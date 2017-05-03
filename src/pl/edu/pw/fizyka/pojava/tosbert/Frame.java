0c5936650c988bf2d8467324a7fe218fc8774ca1
HubertNowakowski <umberto.nowakowski@gmail.com>
2017-05-03 15:13:19 +0200

    new branch commit

--- src/pl/edu/pw/fizyka/pojava/tosbert/Frame.java
+++ src/pl/edu/pw/fizyka/pojava/tosbert/Frame.java
@@ -4,8 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.HeadlessException;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
--- src/pl/edu/pw/fizyka/pojava/tosbert/GraphPanel.java
+++ src/pl/edu/pw/fizyka/pojava/tosbert/GraphPanel.java
@@ -1,11 +1,15 @@
 package pl.edu.pw.fizyka.pojava.tosbert;
 
 import java.awt.Color;
-import java.awt.LayoutManager;
 
 import javax.swing.JPanel;
 
 public class GraphPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public GraphPanel() {
 		
