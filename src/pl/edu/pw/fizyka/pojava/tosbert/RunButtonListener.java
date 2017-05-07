ac8e53e74b0b4e95622383ae43f594c63622f846
AntoninaPater <spiewajacy.pies@gmail.com>
2017-05-07 15:07:43 +0200

    start button animation added

--- src/pl/edu/pw/fizyka/pojava/tosbert/RunButtonListener.java
+++ src/pl/edu/pw/fizyka/pojava/tosbert/RunButtonListener.java
@@ -5,24 +5,26 @@ import java.awt.event.ActionListener;
 
 
 public class RunButtonListener implements ActionListener{
-    Frame frame;
+	Frame frame;
 
-    public RunButtonListener(Frame frame) {
-	this.frame = frame;
-    }
+	public RunButtonListener(Frame frame) {
+		this.frame = frame;
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-	if(this.frame.animation.animationRunning) {
-	    this.frame.animation.animationRunning = false;
-	    System.out.println("Animation stopped.");
-	    this.frame.animation.stopAnimation();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(this.frame.animation.animationRunning) {
+			this.frame.animation.animationRunning = false;
+			System.out.println("Animation stopped.");
+			frame.bottom.runButtonPanel.runButton.setText("START");
+			this.frame.animation.stopAnimation();
+		}
+		else{
+			this.frame.animation.animationRunning = true;
+			System.out.println("Animation started.");
+			frame.bottom.runButtonPanel.runButton.setText("STOP");
+			this.frame.animation.startAnimation();
+		}
 	}
-	else{
-	    this.frame.animation.animationRunning = true;
-	    System.out.println("Animation started.");
-	    this.frame.animation.startAnimation();
-	}
-    }
 
 }
