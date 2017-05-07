e970de324ff5489df2f8313a4092c738682532b0
HubertNowakowski <umberto.nowakowski@gmail.com>
2017-05-07 14:42:14 +0200

    Basic animation of light beam

--- src/pl/edu/pw/fizyka/pojava/tosbert/Frame.java
+++ src/pl/edu/pw/fizyka/pojava/tosbert/Frame.java
@@ -14,87 +14,87 @@ import javax.swing.UIManager;
 
 public class Frame extends JFrame { //Antonina Pater, Hubert Nowakowski
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
 
 
 
-    BottomPanel bottom;
-    AnimationPanel animation;
+	BottomPanel bottom;
+	AnimationPanel animation;
 
-    JMenuBar menuBar;
-    JMenu menu;
-    JMenuItem chartMenu;
-    JMenuItem timeMenu;
+	JMenuBar menuBar;
+	JMenu menu;
+	JMenuItem chartMenu;
+	JMenuItem timeMenu;
 
-    public Frame() throws HeadlessException {
+	public Frame() throws HeadlessException {
 
-	super("Symulacja Efektu Fizeau");
+		super("Symulacja Efektu Fizeau");
 
-	try {
-	    UIManager.setLookAndFeel(
-		    UIManager.getSystemLookAndFeelClassName());
+		try {
+			UIManager.setLookAndFeel(
+					UIManager.getSystemLookAndFeelClassName());
 
-	} catch (Exception e1) {
+		} catch (Exception e1) {
 
-	    e1.printStackTrace();
-	    System.out.println("błąd LF");
+			e1.printStackTrace();
+			System.out.println("błąd LF");
+		}
+		SwingUtilities.updateComponentTreeUI(Frame.this);
+
+		setExtendedState(MAXIMIZED_BOTH); 
+		setUndecorated(false);
+
+		setPreferredSize(new Dimension(1200,900));
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setBackground(Color.WHITE);
+		setLayout(new BorderLayout());
+
+
+		this.menuBar = new JMenuBar();
+		this.menu = new JMenu();
+		this.chartMenu = new JMenuItem();
+		this.timeMenu = new JMenuItem();
+
+		setJMenuBar(this.menuBar);
+		this.menuBar.add(this.menu);
+		this.menu.setText("Menu");
+		this.menu.add(this.chartMenu);
+		this.chartMenu.setText("Pokaż wykres");
+		this.chartMenu.setToolTipText("Wyświetla okno z wykresem");
+		this.menu.add(this.timeMenu);
+		this.timeMenu.setText("Wybierz tempo symulacji");
+		this.timeMenu.setToolTipText("Zmień opcje czasu symulacji, by obserwować bieg promieni świetlnych");
+
+
+
+
+		this.animation = new AnimationPanel(0,600,600);
+
+
+		this.bottom = new BottomPanel();
+
+		this.add(this.animation, BorderLayout.CENTER);
+		this.add(this.bottom, BorderLayout.SOUTH);
+		pack();
+
+		this.bottom.runButtonPanel.runButton.addActionListener(new RunButtonListener(this));
+		this.bottom.settings.velSlider.addChangeListener(new SliderListener(this));
+		this.bottom.settings.distance.addItemListener(new DistanceListener(this));
+		//this.bottom.settings.nTeeth.addItemListener(new ToothListener(this));
 	}
-	SwingUtilities.updateComponentTreeUI(Frame.this);
-	
-	setExtendedState(MAXIMIZED_BOTH); 
-	setUndecorated(false);
-
-	setPreferredSize(new Dimension(1200,900));
-	setDefaultCloseOperation(EXIT_ON_CLOSE);
-	setBackground(Color.WHITE);
-	setLayout(new BorderLayout());
 
 
-	this.menuBar = new JMenuBar();
-	this.menu = new JMenu();
-	this.chartMenu = new JMenuItem();
-	this.timeMenu = new JMenuItem();
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				Frame frame = new Frame();
+				frame.setVisible(true);
+			}
+		});
 
-	setJMenuBar(this.menuBar);
-	this.menuBar.add(this.menu);
-	this.menu.setText("Menu");
-	this.menu.add(this.chartMenu);
-	this.chartMenu.setText("Pokaż wykres");
-	this.chartMenu.setToolTipText("Wyświetla okno z wykresem");
-	this.menu.add(this.timeMenu);
-	this.timeMenu.setText("Wybierz tempo symulacji");
-	this.timeMenu.setToolTipText("Zmień opcje czasu symulacji, by obserwować bieg promieni świetlnych");
-
-
-
-
-	this.animation = new AnimationPanel(0,600,600);
-
-
-	this.bottom = new BottomPanel();
-
-	this.add(this.animation, BorderLayout.CENTER);
-	this.add(this.bottom, BorderLayout.SOUTH);
-	pack();
-
-	this.bottom.runButtonPanel.runButton.addActionListener(new RunButtonListener(this));
-	this.bottom.settings.velSlider.addChangeListener(new SliderListener(this));
-	this.bottom.settings.distance.addItemListener(new DistanceListener(this));
-	//this.bottom.settings.nTeeth.addItemListener(new ToothListener(this));
-    }
-
-
-    public static void main(String[] args) {
-	SwingUtilities.invokeLater(new Runnable() {
-	    @Override
-	    public void run() {
-		Frame frame = new Frame();
-		frame.setVisible(true);
-	    }
-	});
-
-    }
+	}
 }
--- src/pl/edu/pw/fizyka/pojava/tosbert/ToothListener.java
+++ src/pl/edu/pw/fizyka/pojava/tosbert/ToothListener.java
@@ -4,23 +4,21 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 public class ToothListener implements ItemListener{
-    Frame frame;
-    public ToothListener(Frame frame) {
-	this.frame = frame;
-    }
-
-    @Override
-    public void itemStateChanged(ItemEvent e) {
-	if (e.getStateChange() == ItemEvent.SELECTED) {
-	    int n = (Integer)e.getItemSelectable().getSelectedObjects()[0];
-
-	    this.frame.animation.setN(n);
-	    for(WheelTooth t : this.frame.animation.wheelTeeth)
-		//t.resize( t.width, (int) ( this.frame.animation.R * Math.PI/n)  );
-		System.out.println(t.height);
-
-	    this.frame.animation.repaint();
+	Frame frame;
+	public ToothListener(Frame frame) {
+		this.frame = frame;
 	}
-    }
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		if (e.getStateChange() == ItemEvent.SELECTED) {
+			int n = (Integer)e.getItemSelectable().getSelectedObjects()[0];
+
+			this.frame.animation.setN(n);
+			this.frame.animation.makeWheelTeeth( (int)(this.frame.animation.R * Math.PI/n) , frame.animation );
+			this.frame.animation.repaint();
+		}
+	}
 
 }
